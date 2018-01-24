f5bb60c9b7467965b69fbff6e26a81a4066a7da1
ignacio <ignacionf@gmail.com>
2018-01-24 16:01:44 -0300

    notas

--- home/views.py
+++ home/views.py
@@ -21,6 +21,12 @@ class AutorView(DetailView):
     template_name = "autor.html"
     model = Autor
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        autor = Q(libro__autor=context['object'])
+        context['notas'] = Nota.objects.filter(publicado=True).filter(autor)
+        return context
+
 class PrensaView(ListView):
     template_name = "prensa.html"
     model = Nota
@@ -33,8 +39,18 @@ class PrensaView(ListView):
         context['destacados'] = self.queryset.filter(destacado=True)[:10]
         return context
 
+from django.db.models import Q
 class NotaView(DetailView):
     template_name = "nota.html"
     model = Nota
+    queryset = Nota.objects.filter(publicado=True).order_by("-created_at")
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        libros = Q(libro=context['object'].libro)
+        autor = Q(libro__autor=context['object'].libro.autor)
+        medio = Q(medio=context['object'].medio)
+        notas = self.queryset.filter(libros | autor | medio).exclude(pk=context['object'].id)
+        context['notas'] = notas[0:5]
+        return context
 
