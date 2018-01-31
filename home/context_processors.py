346fb572a33383707e145c4d9cf5c5d30dd2cdf4
ignacio <ignacionf@gmail.com>
2018-01-31 11:29:42 -0300

    lala

--- home/context_processors.py
+++ home/context_processors.py
@@ -2,5 +2,5 @@ from .models import Coleccion
 
 def colecciones(request):
     return {
-        'colecciones': Coleccion.objects.filter(orden__lt=90).order_by("orden")
+        'colecciones': Coleccion.objects.filter(orden__lt=9).order_by("orden")
     }
